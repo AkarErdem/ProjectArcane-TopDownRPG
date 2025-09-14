5ae7bbcb103f41ae83eb80c8ee17911d7b06d586
Erdem Akar <erdemakardev@gmail.com>
2025-09-14 17:54:42 +0200

    feat: add globe progress bar

---
+++ Source/Arcane/Private/UI/Widget/ArcaneUserWidget.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2025 Erdem Akar
+
+
+#include "UI/Widget/ArcaneUserWidget.h"
+
+void UArcaneUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	OnWidgetControllerSet();
+}
---
+++ Source/Arcane/Public/UI/Widget/ArcaneUserWidget.h
@@ -0,0 +1,24 @@
+﻿// Copyright (c) 2025 Erdem Akar
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ArcaneUserWidget.generated.h"
+
+UCLASS()
+class ARCANE_API UArcaneUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UObject> WidgetController;
+
+	UFUNCTION(BlueprintCallable)
+	void SetWidgetController(UObject* InWidgetController);
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnWidgetControllerSet();
+};
